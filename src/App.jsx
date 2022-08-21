0313eddc7533fcb69fc87320ac497658a5d5eea7
torik135 <riqibrekele@gmail.com>
2022-08-21 14:25:58 +0700

    layouting inside App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Mainlayout } from './layouts';
+import { Activity, Todo } from './pages';
 
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <Mainlayout>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/detail/:id' element={<Todo />} />
+          <Route path='/' element={<Activity />} />
+        </Routes>
+      </BrowserRouter>
+    </Mainlayout>
+  );
 }
 
 export default App;
