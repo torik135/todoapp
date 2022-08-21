fd6ce46a7c3839fba794c306ee548c42dfcde50e
torik135 <riqibrekele@gmail.com>
2022-08-21 14:24:57 +0700

    add: services

---
+++ src/services/services.jsx
@@ -0,0 +1,101 @@
+const BASE_URL = 'https://todo.api.devcode.gethired.id';
+const EMAIL = 'riqibrekele%40gmail.com';
+
+function useActivity() {
+  const baseUrl = `${BASE_URL}/activity-groups`;
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Cache-Control': 'no-cache',
+  };
+
+  const get = async () => {
+    const res = await fetch(`${baseUrl}?email=${EMAIL}`);
+    return await res.json();
+  };
+
+  const create = async (data) => {
+    const res = await fetch(baseUrl, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(data),
+    });
+
+    return await res.json();
+  };
+
+  const remove = async (id) => {
+    const res = await fetch(baseUrl + '/' + id, {
+      method: 'DELETE',
+      headers,
+    });
+    return await res.json();
+  };
+
+  const update = async (id, data) => {
+    const res = await fetch(baseUrl + '/' + id, {
+      method: 'PATCH',
+      headers,
+      body: JSON.stringify(data),
+    });
+    return await res.json();
+  };
+
+  return {
+    get,
+    create,
+    remove,
+    update,
+  };
+}
+
+function useTodos() {
+  const baseUrl = `${BASE_URL}/todo-items/`;
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Cache-Control': 'no-cache',
+  };
+
+  const get = async (id) => {
+    const res = await fetch(`${BASE_URL}/activity-groups/${id}`);
+    return await res.json();
+  };
+
+  const create = async (data) => {
+    const res = await fetch(baseUrl, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(data),
+    });
+    return await res.json();
+  };
+
+  const remove = async (id) => {
+    const res = await fetch(baseUrl + id, {
+      method: 'DELETE',
+      headers,
+    });
+    return await res.json();
+  };
+
+  const update = async (id, data) => {
+    const res = await fetch(baseUrl + id, {
+      method: 'PATCH',
+      headers,
+      body: JSON.stringify(data),
+    });
+    return await res.json();
+  };
+
+  return {
+    get,
+    create,
+    remove,
+    update,
+  };
+}
+
+export { useActivity, useTodos };
